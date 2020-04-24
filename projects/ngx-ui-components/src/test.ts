728a6da58da4088629cd64b8970cea2ba4ca69f7
Jason Koolman <jason.koolman@hotmail.com>
2020-04-24 23:34:38 +0200

    test: Add global Jasmine matchers

--- projects/ngx-ui-components/src/test.ts
+++ projects/ngx-ui-components/src/test.ts
@@ -12,12 +12,21 @@ declare const require: {
   };
 };
 
+// Add custom matchers globally
+import matchers from './lib/core/testing/matchers';
+
+beforeEach(() => {
+  jasmine.addMatchers(matchers);
+});
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
+
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
+
 // And load the modules.
 context.keys().map(context);
